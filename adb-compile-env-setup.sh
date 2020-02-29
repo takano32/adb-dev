28bc88d11fea43e7c619d93bbbeba5af75d2a1f6
TAKANO Mitsuhiro <takano32@gmail.com>
2020-02-29 23:02:32 +0900

    build android-5.1.1_r38 branch

--- adb-compile-env-setup.sh
+++ adb-compile-env-setup.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
-
 # some steps found here: http://android.serverbox.ch/?p=1217
+#
+
+set -xe
+
+BRANCH=android-5.1.1_r38
+# BRANCH=android-10.0.0_r29
 
 sudo apt-get install libssl-dev
 
-mkdir ~/adb-dev/build-5.1.1
-cd ~/adb-dev/build-5.1.1
+mkdir ~/adb-dev/build-$BRANCH
+cd ~/adb-dev/build-$BRANCH
 
 mkdir system
 mkdir external
 
 cd system/                
-git clone -b android-5.1.1_r1 https://android.googlesource.com/platform/system/core
-git clone -b android-5.1.1_r1 https://android.googlesource.com/platform/system/extras
+git clone -q -b $BRANCH --depth 1 https://android.googlesource.com/platform/system/core
+git clone -q -b $BRANCH --depth 1 https://android.googlesource.com/platform/system/extras
 
 cd ../external/                 
-git clone -b android-5.1.1_r1 https://android.googlesource.com/platform/external/zlib
-git clone -b android-5.1.1_r1 https://android.googlesource.com/platform/external/openssl
-git clone -b android-5.1.1_r1 https://android.googlesource.com/platform/external/libselinux
+git clone -q -b $BRANCH --depth 1 https://android.googlesource.com/platform/external/zlib
+git clone -q -b $BRANCH --depth 1 https://android.googlesource.com/platform/external/openssl
+git clone -q -b $BRANCH --depth 1 https://android.googlesource.com/platform/external/libselinux
 
 cd ../system/core/adb
 
-cp ~/tmp/Makefile .
+cp ~/adb-dev/Makefile.adb ./Makefile
 
+pwd
 echo "Ready to hit *make* now and pray?"
 read -rsp $'Press any key to continue...\n' -n1 key
+
